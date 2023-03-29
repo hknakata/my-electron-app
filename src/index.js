61dacb34fb6e72809193f035a0db4999f380fe0c
hknakata <hknakata@gmail.com>
2023-03-29 23:24:21 +0800

    chore: cleanup & format

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
-const Store = require('electron-store');
+const Store = require("electron-store");
 const store = new Store();
-store.set('isDarkMode', true);
+store.set("isDarkMode", true);
 
-store.onDidChange('isDarkMode', (newValue, oldValue) => {
-  console.log('isDarkMode had changed:', store.get('isDarkMode'));
+store.onDidChange("isDarkMode", (newValue, oldValue) => {
+  console.log("isDarkMode had changed:", store.get("isDarkMode"));
 });
 
 // Import the Express framework
-const express = require('express');
+const express = require("express");
 
 // Create a new Express app instance
 const api = express();
@@ -19,18 +19,22 @@ const api = express();
 const PORT = 3000;
 
 // Define a route for the root endpoint
-api.get('/', (req, res) => {
-    // Return a "This is my first API" message
-    res.send('This is my first API.');
+api.get("/", (req, res) => {
+  // Return a "This is my first API" message
+  res.send("This is my first API.");
 });
 
-api.get('/api/users/:id', (req, res) => {
+api.get("/api/users/:id", (req, res) => {
   // Create a new user
-  const user = { "id": req.params.id, "name": "hello world", "isDarkMode": store.get('isDarkMode')};
-  
+  const user = {
+    id: req.params.id,
+    name: "hello world",
+    isDarkMode: store.get("isDarkMode"),
+  };
+
   // Toogle isDarkMode
-  const isDarkMode = store.get('isDarkMode');
-  store.set('isDarkMode', !isDarkMode);
+  const isDarkMode = store.get("isDarkMode");
+  store.set("isDarkMode", !isDarkMode);
 
   // Return a user json
   res.json(user);
@@ -38,16 +42,15 @@ api.get('/api/users/:id', (req, res) => {
 
 // Start the app by listening on the specified port
 api.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
 
 // Embed the Express app into your main app file
 const expressApp = express();
 expressApp.use(api); // Use the app as middleware
 
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
+if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
@@ -57,12 +60,12 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
@@ -71,18 +74,18 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
